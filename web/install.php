75a9bd43306a50f920c9d915f9d8c79dd4a18bee
cinnam89 <ricardovillalobos8992@gmail.com>
2018-12-20 11:50:22 +0100

    Correcion de las variables en install.php

--- web/install.php
+++ web/install.php
@@ -2,7 +2,7 @@
   include("../app/datosBD/Config.php");
 
   //Nos conectamos a la base de datos
-  $conexion = mysqli_connect($mvc_bd_hostname, $mvc_bd_usuario, $mvc_bd_clave, $mvc_bd_nombre) or die ("<p>No se ha podido establecer la conexion con la base de datos</p>");
+  $conexion = mysqli_connect($servidor, $usuario, $psw, $bd) or die ("<p>No se ha podido establecer la conexion con la base de datos</p>");
 
 	   if($conexion -> query("USE g12alimentos")===TRUE){
 		     printf("Estas usando la base de datos g12alimentos <br>");
